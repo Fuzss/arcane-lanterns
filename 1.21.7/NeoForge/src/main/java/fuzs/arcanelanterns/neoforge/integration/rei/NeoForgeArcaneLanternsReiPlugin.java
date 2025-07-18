19c38461ccd3e195f7683bc0ae41219d7e5905ff
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-07-18 13:54:26 +0200

    prepare 1.21.8 port

--- 1.21.7/NeoForge/src/main/java/fuzs/arcanelanterns/neoforge/integration/rei/NeoForgeArcaneLanternsReiPlugin.java
+++
@@ -1,9 +0,0 @@
-//package fuzs.arcanelanterns.neoforge.integration.rei;
-//
-//import fuzs.arcanelanterns.integration.rei.ArcaneLanternsReiPlugin;
-//import me.shedaniel.rei.forge.REIPluginCommon;
-//
-//@REIPluginCommon
-//public class NeoForgeArcaneLanternsReiPlugin extends ArcaneLanternsReiPlugin {
-//
-//}
